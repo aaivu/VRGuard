22e62797dc5e13d1298850cfcc7b101b25079d13
Vithurabiman Senthuran <180588g@uom.lk>
2023-08-29 00:49:27 +0530

    Initial files

---
+++ feature_extractor/extract_1d_signal.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from scipy.signal import welch
+import neurokit2 as nk
+import numpy as np
+import EntropyHub as EH
+from features.features import Features
+
+
+class FeatureExtractor_1D:
+    def __init__(self, fs=100, batch_size=1):
+        self.fs = fs
+        self.batch_size = batch_size
+
+    def calculate_fuzzy_entropy_batch(self, batch):
+        result = batch.apply(lambda row: Features.calculate_fuzzy_entropy(row), axis=1)
+        return result.values 
+
+
+    def extract_features(self, signals,prnt=False):
+        fuzzy_entropy_values = []
+        for i in range(0,signals.shape[0], self.batch_size):
+            batch = signals.iloc[i:i + self.batch_size,:]
+            batch_fuzzy_entropy = self.calculate_fuzzy_entropy_batch(batch)
+            fuzzy_entropy_values.extend(batch_fuzzy_entropy)
+            if i % 100 == 0 and print:
+                print("Processed {} rows".format(i))
+
+        features=pd.DataFrame(fuzzy_entropy_values)
+        features.columns=["fuzz_en{}".format(i) for i in range(1,len(fuzzy_entropy_values[0])+1)]
+        features["PSD"]=signals.apply(lambda row:Features.compute_psd(row,self.fs),axis=1)
+     
+        features["mean"]= features["PSD"].apply(lambda row:Features.compute_mean(row))
+        features["std"]= features["PSD"].apply(lambda row:Features.compute_std(row))
+        print(features.head())
+
+        return features
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+# Usage
+from features.features import Features
+from feature_extractor.extract_1d_signal import FeatureExtractor_1D
+import pandas as pd
+
+
+if __name__ == "__main__":
+    signals = pd.read_csv("C:\\Users\\USER\\Desktop\\Privacy_Re\\eeg_validation.csv", index_col=0)
+    signals=signals.iloc[0:5]
+    feature_extractor = FeatureExtractor_1D(fs=200, batch_size=100)
+    extracted_features = feature_extractor.extract_features(signals)
+    #print(extracted_features.head())
+    # You can now work with the extracted_features DataFrame
